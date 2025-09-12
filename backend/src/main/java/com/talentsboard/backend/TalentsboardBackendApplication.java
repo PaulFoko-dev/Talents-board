7059c713c5f640abf4884d08343229c79e64d888
PaulFoko-dev <paulfoko@matrix-tech.fr>
2025-09-12 11:42:59 +0200

    Initial commit: Ajout frontend + backend Spring Boot + config MySQL + docker-compose

---
+++ backend/src/main/java/com/talentsboard/backend/TalentsboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.talentsboard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentsboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentsboardBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/talentsboard/backend/TalentsboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentsboard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentsboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
