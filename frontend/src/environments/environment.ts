5242c662aae0f292d55384e170f8a629ae8c63de
tomy <fagninob@3il.fr>
2025-09-16 16:46:05 +0200

    frontend:configuration firebase

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyC4xbxcUTHX7tsnaH7y8lnL6XDWAsVqk0E",
+  authDomain: "talents-board-b6d36.firebaseapp.com",
+  projectId: "talents-board-b6d36",
+  storageBucket: "talents-board-b6d36.firebasestorage.app",
+  messagingSenderId: "516089602610",
+  appId: "1:516089602610:web:66680fbac99fb938c6522b",
+  measurementId: "G-6B6ZEGG08Y"
+ }
+};
