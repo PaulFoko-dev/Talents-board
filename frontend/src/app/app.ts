45a55c64ac000600e0010053f9b1414d77bd19f7
MIKE-TAGNE-3il <tagnefom@3il.fr>
2025-09-22 12:33:24 +0200

    Page d'acceuil milieu
    Je me suis occupe ici des parties: Bloc Fonctionnalités et Comment ça marche

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeaturesGridComponent } from '../app-features-grid.component';
+import { HowItWorksComponent } from '../how-it-works.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FeaturesGridComponent, HowItWorksComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
