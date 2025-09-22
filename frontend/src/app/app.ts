70405a4d71d6f603a2012699832337cb48d3d4da
RickArthurKetch <arthurrick849@gmail.com>
2025-09-22 14:52:22 +0200

    feat(frontend): ajout du Footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Temoignages } from "./components/temoignages/temoignages";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Temoignages, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend/src/app/components/temoignages/temoignages.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-temoignages',
+  imports: [],
+  templateUrl: './temoignages.html',
+  styleUrl: './temoignages.scss'
+})
+export class Temoignages {
+
+  @Input() avis : string = "Grâce à Talents-board, j'ai trouvé mon emploi de rêve en un temps record ! L'interface est intuitive et le matching est d'une efficacité redoutable.";
+  @Input() auteur : string = "Sophie Dupont";
+  @Input() position : string = "Développeuse Full-stack";
+  @Input() photo : string = "Sophie.png"
+
+}
