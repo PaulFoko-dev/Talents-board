6291be2e7bd3a03b8deaa0434306b5879891c854
MIKE-TAGNE-3il <tagnefom@3il.fr>
2025-10-02 12:39:23 +0200

    fix: corrections Angular et configuration routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './View/home/home.component';
 import { Contact } from './View/contact/contact';
+import { ProfilEntreprise } from './features/profil-entreprise/profil-entreprise';
 
 export const routes: Routes = [
-  // Route racine vers la page d'accueil
+  // Route racine vers la page d accueil
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  // Alias explicite vers la page d'accueil
+  // Alias explicite vers la page d accueil
   { path: 'home', component: HomeComponent },
-  {path: 'contact', component: Contact}
+  { path: 'contact', component: Contact },
+  { path: 'profil-entreprise', component: ProfilEntreprise }
 ];
