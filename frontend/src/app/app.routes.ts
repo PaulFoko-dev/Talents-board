1ac81b26490c8ea7d3f43182c861cb3fc67ab25f
junimodo <fotsojunior230@gmail.com>
2025-10-06 17:52:32 +0200

    resolution conflit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,7 +9,11 @@ import { Connexion } from './connexion/connexion';
 export const routes: Routes = [
   // Route racine vers la page d'accueil
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path : 'profil', component: ProfileComponent},
   // Alias explicite vers la page d'accueil
   { path: 'home', component: HomeComponent },
-    { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'connexion', component: Connexion },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent }
 ];
