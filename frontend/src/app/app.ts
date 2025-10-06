63a6225b95d329e7ff2c0a955e5ebcd951e5deb6
RickArthurKetch <arthurrick849@gmail.com>
2025-10-06 15:04:06 +0200

    feat(frontend): ajout de la page connexion

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,16 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FeaturesGridComponent } from './components/featuresgrid/featuresgrid';
-import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
-import { Footer } from "./components/footer/footer";
-import { Temoignages } from "./components/temoignages/temoignages";
-import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Temoignages, Footer, FeaturesGridComponent, HowItWorksComponent, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
