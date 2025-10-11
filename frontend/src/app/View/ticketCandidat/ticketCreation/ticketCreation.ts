203c25158b4736358a70036e34955de757adb0e6
MIKE-TAGNE-3il <tagnefom@3il.fr>
2025-10-11 23:28:52 +0200

    feat(frontend): dynamisme des services tickets candidats

--- frontend/src/app/View/ticketCandidat/ticketCreation/ticketCreation.ts
+++ frontend/src/app/View/ticketCandidat/ticketCreation/ticketCreation.ts
@@ -86,7 +86,7 @@ export class TicketCreation implements OnInit {
   }
 
   onCancel(): void {
-    // Redirection simple vers la page précédente (la liste)
+    // Redirection simple vers la page précédente (la l
     this.router.navigate(['/tickets-candidats']);
   }
 
